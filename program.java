fc033a479e1856fcd9b433d647a64d27435210cb
Amreen <wafaamreen6@gmail.com>
2023-04-04 22:35:46 +0530

    updated

--- program.java
+++ program.java
@@ -10,3 +10,10 @@ int c[][]=new int[3][3];  //3 rows and 3 columns
 //adding and printing addition of 2 matrices    
 for(int i=0;i<3;i++){    
 for(int j=0;j<3;j++){    
+
+c[i][j]=a[i][j]+b[i][j];    //use - for subtraction  
+System.out.print(c[i][j]+" ");    
+}    
+System.out.println();//new line    
+}    
+}}  
