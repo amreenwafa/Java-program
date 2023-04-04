b5eb8b502811a8e0775e19136373faabb6d5b902
Amreen <wafaamreen6@gmail.com>
2023-04-04 22:35:14 +0530

    updated

--- program.java
+++ program.java
@@ -6,4 +6,7 @@ int b[][]={{1,3,4},{2,4,3},{1,2,4}};
 
 //creating another matrix to store the sum of two matrices    
 int c[][]=new int[3][3];  //3 rows and 3 columns  
-    
+
+//adding and printing addition of 2 matrices    
+for(int i=0;i<3;i++){    
+for(int j=0;j<3;j++){    
