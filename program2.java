6385627715969ba9f60640af1d1854a2a5833977
Amreen <wafaamreen6@gmail.com>
2023-04-07 20:16:30 +0530

    updated

---
+++ program2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Palindrome
+{
+    public static void main(String[] args)
+	 {
+         Scanner obj=new Scanner(System.in);
+         int n,sx,sum=o,temp;
+         
