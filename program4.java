48a11eea91bbf0c977aa71189ac4e5673e80171c
Amreen <wafaamreen6@gmail.com>
2023-04-11 19:15:19 +0530

    updated

--- program4.java
+++ program4.java
@@ -1,4 +1,15 @@
 import java.util.Scanner; 
 class ProfitLoss
 {
+    public static void main(String args[])
+         { 
+          Scanner obj=new Scanner (System.in);
+            double cp,sp,profit,loss,profitper,lossper;
+
+           System.out.println("Enter the amount of money you spend on buying the product");
+            cp=obj.nextDouble();
+
+          System.out.println("enter the amount of money you are going to sell the product");
+            sp=obj.nextDouble();
+          
 }
