eae7d2806e3de211e74ca4b5be704ef0b63fd16c
Amreen <wafaamreen6@gmail.com>
2023-04-12 19:15:06 +0530

    updated

--- progrsm4.java
+++ progrsm4.java
@@ -12,3 +12,9 @@
     		System.out.println("Please enter any two number");
     		num1=obj.nextInt();
     		num2=obj.nextInt();
+            System.out.println("Select your choice");
+    		System.out.println("Press1 for addition");
+    		System.out.println("Press 2 for substraction");
+    		System.out.println("Press 3 for multiplication");
+    		System.out.println("Press 4 for division");
+    		x=obj.nextInt();
