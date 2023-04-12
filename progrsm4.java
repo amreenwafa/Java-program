c423024dfce4aa0e4b48a372b7873219b5fba15a
Amreen <wafaamreen6@gmail.com>
2023-04-12 19:13:37 +0530

    updated

---
+++ progrsm4.java
@@ -0,0 +1,8 @@
+// WAP a program of four functional calculator not mainu driven 
+    //Coded By Amreen wafa on 26-7
+    
+    import java.util.Scanner;
+    class FourFunCal
+    {
+    	public static void main (String args[])
+    	{
