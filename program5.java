b517b2dd4d6163acc7730b3c3abf54b981fab81f
Amreen <wafaamreen6@gmail.com>
2023-04-13 19:29:36 +0530

    updated

--- program5.java
+++ program5.java
@@ -4,3 +4,11 @@ class Personal
     string name;
     long pan_no, acc_no;
     float basic_ pay;
+
+    void personal(string n, long p, long a, float b)
+    {
+       name = n;
+       pan_no = p;
+       acc_no = a;
+       basic_sal = b;	  	  	
+    }
