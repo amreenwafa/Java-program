b029cc101b90b4710a2a41220ef58be83080e898
Amreen <wafaamreen6@gmail.com>
2023-04-14 19:46:33 +0530

    updated

---
+++ program6.java
@@ -0,0 +1,10 @@
+public class pattern  
+{  
+    public static void main(String[] args) {  
+    int lines=4;  
+    int i,j;  
+    int space=0;  
+    for(i=0;i<lines;i++){// this loop is used to print lines  
+        for(j=1;j<=space;j++){// this loop is used to print space in a line  
+            System.out.print(" ");  
+        }  
