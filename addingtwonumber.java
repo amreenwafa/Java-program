623ce4c433c99dbbd2b2fe7d260e14956599cf3b
Amreen <wafaamreen6@gmail.com>
2023-04-15 19:48:44 +0530

    updated

--- addingtwonumber.java
+++ addingtwonumber.java
@@ -6,4 +6,9 @@ public class MatrixAdditionExample{
     
     //creating another matrix to store the sum of two matrices    
     int c[][]=new int[3][3];  //3 rows and 3 columns  
-        
+
+        //adding and printing addition of 2 matrices    
+        for(int i=0;i<3;i++)
+        {    
+        for(int j=0;j<3;j++)
+        {    
