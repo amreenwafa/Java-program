c19e211cd7fb5c0790db1f26be6d38b7d9edf9f8
Amreen <wafaamreen6@gmail.com>
2023-04-16 20:39:02 +0530

    updated

---
+++ program7.java
@@ -0,0 +1,12 @@
+class BinarySearchExample{  
+    public static void binarySearch(int arr[], int first, int last, int key){  
+      int mid = (first + last)/2;  
+      while( first <= last ){  
+         if ( arr[mid] < key ){  
+           first = mid + 1;     
+           class BinarySearchExample{  
+            public static void binarySearch(int arr[], int first, int last, int key){  
+              int mid = (first + last)/2;  
+              while( first <= last ){  
+                 if ( arr[mid] < key ){  
+                   first = mid + 1;     
