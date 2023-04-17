fd107e0868a0c6b973f032268d625f8c5944df2c
Amreen <wafaamreen6@gmail.com>
2023-04-17 06:39:54 +0530

    updated

--- pattern_problem.java
+++ pattern_problem.java
@@ -4,3 +4,7 @@ public class pattern
     int lines=4;  
     int i,j;  
     int space=0; 
+    for(i=0;i<lines;i++){// this loop is used to print lines  
+        for(j=1;j<=space;j++){// this loop is used to print space in a line  
+            System.out.print(" ");  
+        }  
