787b879663e6abae526dc408c9b477b2b5102219
Amreen <wafaamreen6@gmail.com>
2023-04-18 20:05:04 +0530

    updated

---
+++ programpl.java
@@ -0,0 +1,17 @@
+//Program of matrix of 2x2 oder     
+
+class  MatrixAdd
+{
+	public static void main( String args[ ] )
+	{
+		int  i, j;                 //Declaration of variable
+		int  x[ ][ ] = new int[3][3];
+		
+		int  a[ ][ ] = {  
+					                    {1, 2},                      //1st matrix
+				 	                   {1,1},
+				 	          };
+		int  b[ ][ ] = {  
+					                   {1,1},                        //2nd matrix
+				 	                  {1,1},
+				 	         };
