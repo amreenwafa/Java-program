c6eb744207c7ad4d77e8d2ee60ce83e813ffcbf8
Amreen <wafaamreen6@gmail.com>
2023-04-19 09:11:05 +0530

    updated

--- programSS.java
+++ programSS.java
@@ -1,3 +1,7 @@
 import java.util.Scanner;
 class SeriesSum
 {
+    public static void main (String [] args)
+	   { 
+	          Scanner obj = new Scanner (System.in);
+	            int sum=0,num, i=1;
