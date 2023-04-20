611742ec30aadbbcc423fa56bae4d3e0f8052a73
Amreen <wafaamreen6@gmail.com>
2023-04-20 22:58:30 +0530

    updated

--- program9.java
+++ program9.java
@@ -5,3 +5,7 @@ public static void main(String[] args)
     int number = 987654, reverse = 0;  
 while(number != 0)   
 {  
+    int remainder = number % 10;  
+reverse = reverse * 10 + remainder;  
+number = number/10;  
+}  
