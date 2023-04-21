e05504ba6d89a2bc8549546e289f247a90146fb4
Amreen <wafaamreen6@gmail.com>
2023-04-21 19:48:56 +0530

    updated

---
+++ program10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class FourCal
+{
+	  public static void main (String args[])
+	  {
+        Scanner obj=new Scanner(System.in);
+	  	int x,y,ch,z;
+	  	
+	  	System.out.println("Enter any two number");
+	  	x=obj.nextInt();
+	  	y=obj.nextInt();
+	  	do
+	  	{
