89cca497957a117eeb1df195c4ce4637a4af51c4
Amreen <wafaamreen6@gmail.com>
2023-04-22 17:26:47 +0530

    updated

--- program11.java
+++ program11.java
@@ -4,3 +4,14 @@ import java.util.Scanner;
 //create ATMExample class to implement the ATM functionality  
 public class ATMExample  
 { 
+      //main method starts   
+      public static void main(String args[] )  
+      {  
+          //declare and initialize balance, withdraw, and deposit  
+          int balance = 100000, withdraw, deposit;  
+            
+          //create scanner class object to get choice of user  
+          Scanner sc = new Scanner(System.in);  
+            
+          while(true)  
+          {  
