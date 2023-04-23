770cc1b5899e36439adbee4390dd8aa943fd5ff5
Amreen <wafaamreen6@gmail.com>
2023-04-23 15:29:14 +0530

    updated

--- program12.java
+++ program12.java
@@ -9,3 +9,18 @@ public class InsertEnd {
             this.data = data;  
             this.next = null;  
         }  
+    }  
+  
+    //Represent the head and tail of the singly linked list  
+    public Node head = null;  
+    public Node tail = null;  
+  
+    //addAtEnd() will add a new node to the end of the list  
+    public void addAtEnd(int data) {  
+        //Create a new node  
+        Node newNode = new Node(data);  
+  
+        //Checks if the list is empty  
+        if(head == null) {  
+            //If list is empty, both head and tail will point to new node  
+            head = newNode;  
