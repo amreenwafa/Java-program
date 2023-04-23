8789e970108e80f16f5993e41671643cd97a653e
Amreen <wafaamreen6@gmail.com>
2023-04-23 15:31:10 +0530

    updated

--- program12.java
+++ program12.java
@@ -33,4 +33,17 @@ public class InsertEnd {
             tail = newNode;  
         }  
     }  
-  
+   //display() will display all the nodes present in the list  
+   public void display() {  
+    //Node current will point to head  
+    Node current = head;  
+    if(head == null) {  
+        System.out.println("List is empty");  
+        return;  
+    }  
+    System.out.println("Adding nodes to the end of the list: ");  
+    while(current != null) {  
+        //Prints each node by incrementing pointer  
+        System.out.print(current.data + " ");  
+        current = current.next;  
+    }  
