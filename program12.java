4d82a61121e1e4f6e53dec8a1d0031d52c22bd6c
Amreen <wafaamreen6@gmail.com>
2023-04-23 15:29:56 +0530

    updated

--- program12.java
+++ program12.java
@@ -24,3 +24,13 @@ public class InsertEnd {
         if(head == null) {  
             //If list is empty, both head and tail will point to new node  
             head = newNode;  
+            tail = newNode;  
+        }  
+        else {  
+            //newNode will be added after tail such that tail's next will point to newNode  
+            tail.next = newNode;  
+            //newNode will become new tail of the list  
+            tail = newNode;  
+        }  
+    }  
+  
