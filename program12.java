fa18881cf5d147a4194fdb6a4f3db58784a4c348
Amreen <wafaamreen6@gmail.com>
2023-04-23 15:27:09 +0530

    updated

---
+++ program12.java
@@ -0,0 +1,11 @@
+public class InsertEnd {  
+  
+    //Represent a node of the singly linked list  
+    class Node{  
+        int data;  
+        Node next;  
+  
+        public Node(int data) {  
+            this.data = data;  
+            this.next = null;  
+        }  
