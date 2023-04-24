df34b5ac6e83da85faa5c6bc6e841a17f5511865
Amreen <wafaamreen6@gmail.com>
2023-04-24 18:41:28 +0530

    updated

--- program14.java
+++ program14.java
@@ -2,4 +2,10 @@ public class OddEven
 {    
     public static void main(String[] args) {    
         int rows, cols, countOdd = 0, countEven = 0;    
-            
+              //Initialize matrix a    
+        int a[][] = {       
+            {4, 1, 3},    
+            {3, 5, 7},    
+            {8, 2, 6}    
+        };    
+  
