24dfec66d623dd30374d19c93035d94478409aba
Amreen <wafaamreen6@gmail.com>
2023-04-25 23:15:54 +0530

    updated

---
+++ program17.java
@@ -0,0 +1,12 @@
+public class pattern  
+{    
+     public static void main(String[] args){    
+    int lines=4;    
+    int i,j;    
+    for(i=1;i<=lines;i++){// this loop is used to print lines    
+      for(j=1;j<=lines;j++){// this loop is used to print * in a line    
+          if(i==j)    
+             System.out.print("*");    
+            else    
+           System.out.print("0");    
+      }    
