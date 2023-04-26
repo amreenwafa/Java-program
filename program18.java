74c9db80c18e965d258bb9f042c16102060fa6b5
Amreen <wafaamreen6@gmail.com>
2023-04-26 21:31:24 +0530

    updated

---
+++ program18.java
@@ -0,0 +1,11 @@
+public class LongestRepeatingSequence {  
+    //Checks for the largest common prefix  
+    public static String lcp(String s, String t){  
+        int n = Math.min(s.length(),t.length());  
+        for(int i = 0; i < n; i++){  
+            if(s.charAt(i) != t.charAt(i)){  
+                return s.substring(0,i);  
+            }  
+        }  
+        return s.substring(0,n);  
+    }  
