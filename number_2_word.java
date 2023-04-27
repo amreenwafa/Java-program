f5413d19990c70b321782fc0a8bbfab33ff814a0
Amreen <wafaamreen6@gmail.com>
2023-04-27 12:55:50 +0530

    updated

--- number_2_word.java
+++ number_2_word.java
@@ -2,3 +2,6 @@ class NumberToWordExample1
 {  
 //user-defined static method that converts a number into words  
 static void numberToWords(char num[]) 
+{  
+    //determines the number of digits in the given number  
+    int len = num.length;  
