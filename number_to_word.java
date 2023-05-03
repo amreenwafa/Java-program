b93f5784b4cda8330a5aa97d916fae6244d3d178
Amreen <wafaamreen6@gmail.com>
2023-05-03 15:27:27 +0530

    updated

--- number_to_word.java
+++ number_to_word.java
@@ -1,3 +1,8 @@
 import java.text.DecimalFormat;  
 public class NumberToWordExample2   
 {  
+    //string type array for one digit numbers     
+private static final String[] twodigits = {"", " Ten", " Twenty", " Thirty", " Forty", " Fifty", " Sixty", " Seventy", " Eighty", " Ninety"};  
+//string type array for two digits numbers   
+private static final String[] onedigit = {"", " One", " Two", " Three", " Four", " Five", " Six", " Seven", " Eight", " Nine", " Ten", " Eleven", " Twelve", " Thirteen", " Fourteen", " Fifteen", " Sixteen", " Seventeen", " Eighteen", " Nineteen"};  
+//defining constructor of the class
