1dc911c478f07e870f13da76eaeb17633eec5ea4
Amreen <wafaamreen6@gmail.com>
2023-05-07 11:29:08 +0530

    updated

---
+++ java coding Sem5/Arraylist1.java
@@ -0,0 +1,18 @@
+//Arraylist program using add method
+import java.util.ArrayList;
+class Arraylist1
+{
+  public static void main(String args[])
+ {
+     ArrayList<String> car = new ArrayList<String>();
+ 
+    //add element
+      car.add("Volvo");
+      car.add("Ford");
+      car.add("suzuki");
+      car.add("BMW");
+
+          System.out.println(car);
+ }
+
+}
