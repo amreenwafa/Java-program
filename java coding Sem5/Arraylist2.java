99e3430742d646241e2a56da1191d0910a669de3
Amreen <wafaamreen6@gmail.com>
2023-05-07 14:09:56 +0530

    updated

--- java coding Sem5/Arraylist2.java
+++ java coding Sem5/Arraylist2.java
@@ -7,19 +7,22 @@ class Arraylist2
   public static void main(String args[])
   {
     Scanner sc =new Scanner(System.in);
-   ArrayList <String> n = new ArrayList<String>();
-    
+   ArrayList<String> n = new ArrayList<String>();
+   String name; 
+   
     for(int i=0; i<10; i++)
     {
        System.out.println("Enter the name of student");
-       String name = sc.nextLine();
-       n.add(); 
+       name = sc.nextLine();  
+       n.add(name); 
        
     } 
     Collections.sort(n);
       
-       System.out.println(n);
-      
+       for(int i=0; i<10; i++)
+     {
+       System.out.println(n.get(i));
+     }
     
   }
 
