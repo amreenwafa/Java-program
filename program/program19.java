610f61e8e98a7c5f2f84276aaa8756934e97c0b1
Amreen <wafaamreen6@gmail.com>
2023-05-10 19:18:04 +0530

    updated

--- program/program19.java
+++ program/program19.java
@@ -3,6 +3,10 @@ public class program19 {
 	  {
         Scanner obj=new Scanner(System.in);
 	  	int x,y,ch,z;
-	  	
+	  	System.out.println("Enter any two number");
+	  	x=obj.nextInt();
+	  	y=obj.nextInt();
+	  	do
+	  	{
     
 }
