0a3c7f266777892bf748ab357e112bb1161be5e7
Amreen <wafaamreen6@gmail.com>
2023-05-10 19:18:33 +0530

    updated

--- program/program19.java
+++ program/program19.java
@@ -8,5 +8,12 @@ public class program19 {
 	  	y=obj.nextInt();
 	  	do
 	  	{
+            System.out.println("Select your choice");
+	      System.out.println("Press 1 for addition");
+	  	System.out.println("Press 2 for substracton"); 
+	  	System.out.println("Press 3 for multiplication");
+	  	System.out.println("Press 4 for division");
+	  	ch=obj.nextInt();
+	  	
     
 }
