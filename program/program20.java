3ce87884fca72ffa9fa45bdb2b0c7f7c704f9131
Amreen <wafaamreen6@gmail.com>
2023-05-11 08:10:19 +0530

    updated

---
+++ program/program20.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class BankAccount                    //Super class
+ {
+	Scanner obj=new Scanner(System.in);
+	
+	      String name;
+	      double acc_no;
+	      float dep, wdrl, bal=1000;
+	      long num;
