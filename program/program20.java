b15f40aa4c7de8879503c731cc47f762a8f6c63c
Amreen <wafaamreen6@gmail.com>
2023-05-11 08:10:53 +0530

    updated

--- program/program20.java
+++ program/program20.java
@@ -7,3 +7,14 @@ class BankAccount                    //Super class
 	      double acc_no;
 	      float dep, wdrl, bal=1000;
 	      long num;
+          
+          void input()                    //Display
+	      {
+	      	System.out.println("Enter your name");
+	      	name=obj.nextLine();
+	      	System.out.println("Enter your account number");
+	      	acc_no=obj.nextDouble();
+	      	System.out.println("Enter  mobile number");
+	      	num=obj.nextLong();
+	      }
+	      
