12e6cd94df7c1a64699dd27e8d3afdfa6aadca9c
Amreen <wafaamreen6@gmail.com>
2023-05-11 08:11:22 +0530

    updated

--- program/program20.java
+++ program/program20.java
@@ -7,7 +7,7 @@ class BankAccount                    //Super class
 	      double acc_no;
 	      float dep, wdrl, bal=1000;
 	      long num;
-          
+
           void input()                    //Display
 	      {
 	      	System.out.println("Enter your name");
@@ -17,4 +17,18 @@ class BankAccount                    //Super class
 	      	System.out.println("Enter  mobile number");
 	      	num=obj.nextLong();
 	      }
+	      void display()                    //Display
+	      {
+	      	System.out.println("Your name is" +name);
+	      	System.out.println("Your account number is"   +acc_no);
+	      	System.out.println("Your mobile number is"    +num);
+	      	System.out.println("Your current balance is"   +bal);
+	      }
 	      
+	      void deposit()                     //Deposit
+	      {
+	      	System.out.println("Kindly enter the amount of money you want to deposit");
+	      	dep=obj.nextFloat();
+	      	bal=bal+dep;
+	      	System.out.println("Your total ammout of money after deposit is"   +bal);
+	  	}
