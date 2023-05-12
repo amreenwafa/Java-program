a9daba95a3eaf73792d6e2dc0eb2a1632b4d853b
Amreen <wafaamreen6@gmail.com>
2023-05-12 21:28:23 +0530

    updated

--- program/program21.java
+++ program/program21.java
@@ -5,3 +5,9 @@ class public static void main(String args[])
     {
       Scanner obj=new Scanner (System.in);
        int n,temp,a=0,r;
+       System.out.println("Enter any number");
+         n=obj.nextInt();
+           
+       temp=n;
+      while(n>0)
+      {
