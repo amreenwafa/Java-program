5a4f62830347658323704792f622b643f30f58e2
Amreen <wafaamreen6@gmail.com>
2023-05-13 21:32:37 +0530

    updated

--- program/program22.java
+++ program/program22.java
@@ -8,4 +8,8 @@ class GreateatNum
              System.out.println("Enter two number");
           a=obj.nextInt();
           b=obj.nextInt();
+      
+      if(a>b)
+      System.out.println("first number is the greatest");
+
      
