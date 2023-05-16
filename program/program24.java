ec5e75c4a2ae3e6b50004c7fe94b38ab5e9f11a4
Amreen <wafaamreen6@gmail.com>
2023-05-16 19:33:53 +0530

    updated

--- program/program24.java
+++ program/program24.java
@@ -15,4 +15,15 @@ public class ClothDis
 		System.out.println("total amount of money=" +tot);
 		switch
 		{
+            		case amount1
+		if(amount1>=1&& amount2<=100)
+		dis=amount1;
+		else if(amount1>=101&&amount1<=200)
+		dis=amount1-(5.00/100.00)*amount1;
+		else if(amount1>=201&&amount1<=300)
+		dis=amount1-(7.5/100.00)*amount1;
+		else
+		dis=amount1(10.00/100.00)*amount1;
+		break;
+		
 		
