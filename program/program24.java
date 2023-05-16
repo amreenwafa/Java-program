2c999a32755d617ab2416b704b8e7d178e16ea6f
Amreen <wafaamreen6@gmail.com>
2023-05-16 19:32:51 +0530

    updated

--- program/program24.java
+++ program/program24.java
@@ -3,3 +3,10 @@ public class ClothDis
  {
 	public static void main(String[] args)
 	 {
+        Scanner obj=new Scanner(System.in);
+		int  amount1, amount2;
+		float dis;
+		System.out.println("choose the item and enter the amount of money you spent");
+		System.out.println("Enter the amout of money purchesed in mill cloth");
+		amount1=obj.nextInt();
+		
