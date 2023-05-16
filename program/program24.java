4981457ab64a4f7c6d9970e996c1695ee4361731
Amreen <wafaamreen6@gmail.com>
2023-05-16 19:33:24 +0530

    updated

--- program/program24.java
+++ program/program24.java
@@ -9,4 +9,10 @@ public class ClothDis
 		System.out.println("choose the item and enter the amount of money you spent");
 		System.out.println("Enter the amout of money purchesed in mill cloth");
 		amount1=obj.nextInt();
+        System.out.println("Enter the amount of money you spent on handloom clothing");
+		amount2=obj.nextInt();
+		tot=amount1+amount2;
+		System.out.println("total amount of money=" +tot);
+		switch
+		{
 		
