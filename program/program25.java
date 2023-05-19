f006af8422936d3aebe240200fbfaf36a45bbcc4
Amreen <wafaamreen6@gmail.com>
2023-05-19 19:28:54 +0530

    updated

--- program/program25.java
+++ program/program25.java
@@ -1,3 +1,9 @@
 import java.util.Scanner;
 class Factorial
 {
+    
+	  public static void main (String args[])
+	   { 
+	          Scanner obj = new Scanner (System.in);
+	            int n,x,fact=1;
+	    
