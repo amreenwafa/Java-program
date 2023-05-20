6b5802cde37bd8e15d99388a06c72c807f5e275a
Amreen <wafaamreen6@gmail.com>
2023-05-20 21:08:09 +0530

    updated

--- program/program26.java
+++ program/program26.java
@@ -5,3 +5,11 @@ class Admission
         int m1, m2, m3, m4, m5;
         float per,tMarks=500.0f;
         Scanner in = new Scanner(System.in);
+ //Input
+              void input() 
+              {
+                System.out.println("Enter Your Name");
+                Name = in.nextLine();
+
+                System.out.println("Enter the marks of Subject 1 : ");
+                m1 = in.nextInt();
