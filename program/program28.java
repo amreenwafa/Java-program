42653823596f3102ed2c023509b253a149f584b4
Amreen <wafaamreen6@gmail.com>
2023-05-23 20:00:39 +0530

    updated

--- program/program28.java
+++ program/program28.java
@@ -2,6 +2,18 @@
 
 class  MatrixAdd
 {
-	public static void main( String args[ ] ) //19
+	public static void main( String args[ ] ) //matrix
 	{
+        int  i, j;                 //Declaration of variable
+		int  x[ ][ ] = new int[3][3];
+		
+		int  a[ ][ ] = {  
+					                    {1, 2},                      //1st matrix
+				 	                   {1,1},
+				 	          };
+		int  b[ ][ ] = {  
+					                   {1,1},                        //2nd matrix
+				 	                  {1,1},
+				 	         };
+
 	
