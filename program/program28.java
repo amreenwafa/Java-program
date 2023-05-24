7f7f2daad33f80b7b34a7adf0ab16c3e743349a0
Amreen <wafaamreen6@gmail.com>
2023-05-24 21:29:35 +0530

    updated

--- program/program28.java
+++ program/program28.java
@@ -36,5 +36,11 @@ class  MatrixAdd
 import java.util.Scanner;
 class Factorial
 {
-    
+    	  public static void main (String args[])
+	   { 
+	          Scanner obj = new Scanner (System.in);
+	            int n,x,fact=1;
+	    System.out.println("Enter any number");
+	              n=obj.nextInt();
+	              
 	
