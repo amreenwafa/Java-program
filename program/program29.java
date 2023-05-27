b9c5f72ac9a1f3e4e29a9b7ba47edf747b7849cb
Amreen <wafaamreen6@gmail.com>
2023-05-27 22:50:23 +0530

    updated

--- program/program29.java
+++ program/program29.java
@@ -39,3 +39,11 @@ class Admission
                 }
                    else if(m1<45||m2<45||m3<45||m4<45||m5<45)
                 {
+
+                     System.out.println("Your individual subject marks are less ,You are not eligible as per the guidelines ");
+                }
+                 else
+                {
+                   per = ((m1+m2+m3+m4+m5)/tMarks)*100.0f;
+                   
+                       if(per>=80)
