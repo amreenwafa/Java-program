e9a3703531ea1853e65231b56864eb0cd68ca97d
Amreen <wafaamreen6@gmail.com>
2023-06-02 21:38:12 +0530

    updated

---
+++ program/program30.java
@@ -0,0 +1,8 @@
+public class program30 {
+
+    public static String lcp(String s, String t){  
+        int n = Math.min(s.length(),t.length());  
+        for(int i = 0; i < n; i++){  
+            if(s.charAt(i) != t.charAt(i)){
+    
+}
