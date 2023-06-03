c2ed0f3e37daa88e28049272164552760932ebe4
Amreen <wafaamreen6@gmail.com>
2023-06-03 20:22:48 +0530

    updated

--- program/program30.java
+++ program/program30.java
@@ -4,6 +4,20 @@ public class program30 {
         float per,tMarks=500.0f;
         Scanner in = new Scanner(System.in);
  //Input
+ void input() 
+              {
+                System.out.println("Enter Your Name");
+                Name = in.nextLine();
+
+                System.out.println("Enter the marks of Subject 1 : ");
+                m1 = in.nextInt();
+
+                System.out.println("Enter the marks of Subject 2 : ");
+                m2 = in.nextInt();
+
+                System.out.println("Enter the marks of Subject 3 : ");
+                m3 = in.nextInt();
+
 
 
 
