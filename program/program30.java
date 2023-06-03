3cd764f97aab459959a152cc9cc75c4694881569
Amreen <wafaamreen6@gmail.com>
2023-06-03 20:25:33 +0530

    updated

--- program/program30.java
+++ program/program30.java
@@ -19,7 +19,33 @@ public class program30 {
                 m3 = in.nextInt();
 
 
+                System.out.println("Enter the marks of Subject 4 : ");
+                m4 = in.nextInt();
 
+                System.out.println("Enter the marks of Subject 5 : ");
+                m5 = in.nextInt();
+              }
+
+                     void meritList() 
+             {
+
+                if(m1>100||m2>100||m3>100||m4>100||m5>100) 
+                {
+                        System.out.println("You have entered invalid marks. Please re-enter all marks properly");
+                        input();
+                        meritList();
+                }
+                   else if(m1<45||m2<45||m3<45||m4<45||m5<45)
+                {
+
+                     System.out.println("Your individual subject marks are less ,You are not eligible as per the guidelines ");
+                }
+                 else
+
+  {
+                   per = ((m1+m2+m3+m4+m5)/tMarks)*100.0f;
+                   
+                       if(per>=80)
 
 
 }
