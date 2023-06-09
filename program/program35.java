7c97c2b0cd81e27de00c4bb4c32f17f6c5a12082
Amreen <wafaamreen6@gmail.com>
2023-06-09 21:10:13 +0530

    updated

---
+++ program/program35.java
@@ -0,0 +1,8 @@
+class  MatrixAdd
+{
+	public static void main( String args[ ] ) //matrix
+	{
+        int  i, j;                 //Declaration of variable
+		int  x[ ][ ] = new int[3][3];
+		
+		int  a[ ][ ] = {  
