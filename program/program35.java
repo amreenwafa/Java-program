838769c321686ccb84dff3978ebeb698dba5b13e
Amreen <wafaamreen6@gmail.com>
2023-06-09 21:10:48 +0530

    updated

--- program/program35.java
+++ program/program35.java
@@ -6,3 +6,10 @@ class  MatrixAdd
 		int  x[ ][ ] = new int[3][3];
 		
 		int  a[ ][ ] = {  
+            					                    {1, 2},                      //1st matrix
+				 	                   {1,1},
+				 	          };
+		int  b[ ][ ] = {  
+					                   {1,1},                        //2nd matrix
+				 	                  {1,1},
+				 	         };
