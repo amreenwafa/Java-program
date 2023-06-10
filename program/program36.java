20957b1f8ad850d563d0bd6864c9618b5783f6f4
Amreen <wafaamreen6@gmail.com>
2023-06-10 22:59:12 +0530

    updated

---
+++ program/program36.java
@@ -0,0 +1,9 @@
+public class program36 {
+     String Name;
+        int m1, m2, m3, m4, m5;
+        float per,tMarks=500.0f;
+        Scanner in = new Scanner(System.in);
+ //Input
+ void input() 
+  {
+}
